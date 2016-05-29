10649e0a1268d66cf00cc747442a8135cf56e8e2
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-05-29 12:21:20 +0300

    XML version of the Java configuration for data source.

--- src/main/java/com/git/spring/data/Application.java
+++ src/main/java/com/git/spring/data/Application.java
@@ -2,6 +2,7 @@ package com.git.spring.data;
 
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -12,6 +13,7 @@ public class Application {
         //Bootstrap the configuration to our application.
         try ( AnnotationConfigApplicationContext context =
                       new AnnotationConfigApplicationContext(DataConfiguration.class)) {
+            //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
             BookService service = context.getBean(BookService.class);
             Book book = new Book("First Book", new Date(), 33, new BigDecimal("26.00"));
 
