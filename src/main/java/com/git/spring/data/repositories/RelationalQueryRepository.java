326ad4bf0a11e017de4f803255a72f21165f11b8
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-05-29 15:45:27 +0300

    Added date comparison queries.

--- src/main/java/com/git/spring/data/repositories/RelationalQueryRepository.java
+++ src/main/java/com/git/spring/data/repositories/RelationalQueryRepository.java
@@ -4,12 +4,12 @@ import com.git.spring.data.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface RelationalQueryRepository extends JpaRepository<Book, Long> {
 
-    /** RELATIONAL OPERATORS **/
     /** CAN ONLY BE USED WHEN THE FIELD HAS IMPLEMENTED THE COMPARABLE INTERFACE **/
     //Finds a value that's equal to the one supplied.
     List<Book> findByPageCountEquals(int pageCount);
@@ -28,4 +28,13 @@ public interface RelationalQueryRepository extends JpaRepository<Book, Long> {
 
     //In between the provided values
     List<Book> findByPageCountBetween(int min, int max);
+
+    /** DATE COMPARISON **/
+    /**
+     * Date date = new SimpleDateFormat("MM/dd/yyyy").parse("10/22/1995");
+     * findByPublishDateAfter(date);
+     * **/
+    List<Book> findByPublishDateAfter(Date date);
+    List<Book> findByPublishDateBefore(Date date);
+    List<Book> findByPublishDateBetween(Date date, Date date2);
 }
