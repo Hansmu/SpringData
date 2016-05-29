4d4100b514e6cad746c876adc30c29047a550a54
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-05-29 15:45:53 +0300

    Added result manipulation. Ascending, descending, limiting

--- src/main/java/com/git/spring/data/repositories/DerivedQueriesRepository.java
+++ src/main/java/com/git/spring/data/repositories/DerivedQueriesRepository.java
@@ -4,6 +4,7 @@ import com.git.spring.data.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,4 +34,17 @@ public interface DerivedQueriesRepository extends JpaRepository<Book, Long> {
 
     //findByTitleIgnoreCase("ANIMAL FARM")
     List<Book> findByTitleIgnoreCase(String title);
+
+    /** ORDERING RESULTS **/
+    List<Book> findByTitleContainingOrderByTitleAsc(String title);
+    List<Book> findByTitleContainingOrderByTitleDesc(String title);
+
+    /** LIMITING RESULTS **/
+    //Top - return the first result within the result set. First serves the same purpose. Pick one and stick to it.
+    //Order all of the books by their page count in descending order and return the first item in the list.
+    List<Book> findTopByOrderByPageCountDesc();
+    List<Book> findFirstByOrderByPageCountAsc();
+
+    //Specify some integer value for how many results should be returned
+    List<Book> findTop5ByOrderByPriceDesc();
 }
