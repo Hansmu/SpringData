c872959371c2d9b8e28127ce5ae34fcd2070e9f5
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-06-11 13:42:05 +0300

    Custom query example.

--- src/main/java/com/git/spring/data/repositories/BaseRepository.java
+++ src/main/java/com/git/spring/data/repositories/BaseRepository.java
@@ -1,8 +1,11 @@
 package com.git.spring.data.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
@@ -12,4 +15,9 @@ public interface BaseRepository <T, ID extends Serializable> extends JpaReposito
 
     @Async //To run method executions as asynchronous.
     public List<T> findByIds(ID... ids);
+
+    @Transactional//Then our query is executed within a transaction.
+    @Modifying//Indicator to Spring Data that the method will modify entities.
+    @Query("UPDATE Book b SET b.pageCount = ?2 WHERE b.title LIKE ?1")//JPQL query
+    public int setPageCount(String title, int pageCount);
 }
