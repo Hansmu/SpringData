43d770400faa5dabe4d5dabbc38b95d315780c15
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-06-11 13:29:12 +0300

    Async method.

--- src/main/java/com/git/spring/data/repositories/BaseRepository.java
+++ src/main/java/com/git/spring/data/repositories/BaseRepository.java
@@ -2,6 +2,7 @@ package com.git.spring.data.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.scheduling.annotation.Async;
 
 import java.io.Serializable;
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 @NoRepositoryBean //Not made into a repository.
 public interface BaseRepository <T, ID extends Serializable> extends JpaRepository<T, ID> {
 
+    @Async //To run method executions as asynchronous.
     public List<T> findByIds(ID... ids);
 }
